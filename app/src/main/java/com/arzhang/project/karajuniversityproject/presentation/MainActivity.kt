94b87230170458567a1323b1ce9ff9c327968ecd
arjang_ap <arjang.ap@gmail.com>
2023-11-17 17:04:01 +0330

    add missing package

--- app/src/main/java/com/arzhang/project/karajuniversityproject/presentation/MainActivity.kt
+++ app/src/main/java/com/arzhang/project/karajuniversityproject/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.arzhang.project.karajuniversityproject
+package com.arzhang.project.karajuniversityproject.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
