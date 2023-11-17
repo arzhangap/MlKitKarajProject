5554d0bde4e426475b2c6e9a525225dc03abedd8
arjang_ap <arjang.ap@gmail.com>
2023-11-17 16:59:44 +0330

    first commit

---
+++ app/src/main/java/com/arzhang/project/karajuniversityproject/MainApplication.kt
@@ -0,0 +1,7 @@
+package com.arzhang.project.karajuniversityproject
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application()
